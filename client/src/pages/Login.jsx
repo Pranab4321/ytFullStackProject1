552fcdc5fd63314da28020ae489b78d2b5bffad8
Pranab <mahanandapranab@gmail.com>
2026-02-04 12:42:12 +0530

    Login page is made

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,88 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 export const Login = () => {
+
+  const [user, setUser] = useState({
+    email:"",
+    password:"",
+  });
+
+  const handleInput = (e) =>{
+    const name = e.target.name
+    const value = e.target.value
+
+    setUser({
+      ...user,
+      [name] : value
+    })
+  }
+
+  const hanldeSubmit = (e) =>{
+    e.preventDefault();
+    console.log(user);
+  }
+
   return (
-    <div>Login Page</div>
-  )
-}
+    <>
+      <section>
+        <main>
+          <div className="section-registration">
+            <div className="container grid grid-two-cols">
+              <div className="registration-image">
+                <img
+                  src="/images/atLogin.jpg"
+                  alt="Lets fill the login form."
+                  width="400"
+                  height="400"
+                />
+              </div>
+
+              {/* lets tackle registration form */}
+
+              <div className="registration-form">
+                <h1 className="main-heading mb-3">login form</h1>
+                <br />
+                <form onSubmit={hanldeSubmit}>
+
+                  <div>
+                    <label htmlFor="email">Email</label>
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="Enter email"
+                      id="email"
+                      required
+                      autocomplete="off"
+                      value={user.email}
+                      onChange={handleInput}
+                    />
+                  </div>
+
+                  <div>
+                    <label htmlFor="password">Password</label>
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="Enter password"
+                      id="password"
+                      required
+                      autocomplete="off"
+                      value={user.password}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <br />
+
+                  <button type="submit" className="btn btn-submit">
+                    Register Now
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </main>
+      </section>
+    </>
+  );
+};
